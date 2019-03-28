d8a3ff7b3d4e58468f868cadc1ff84e216996ddd
j5-jarvis <joseph2.jarvis@live.uwe.ac.uk>
2019-03-28 10:17:28 +0000

    updating java script and meta tags

--- script/gallery.js
+++ script/gallery.js
@@ -1,5 +1,3 @@
-
-<script>
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -28,4 +26,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-</script>
